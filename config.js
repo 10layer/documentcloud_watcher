5dde80e43f5467f2c2c3b248941357de0fa2351a
Jason Norwood-Young <jason@10layer.com>
2014-11-20 09:05:27 +0200

    Example config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	dc_username: "<your username here>",
+	dc_password: "<your password here>",
+	dc_url: '<eg. https://sourceafrica.net or https://documentcloud.org>',
+	mongo_connect_string: 'mongodb://localhost/<your database name>',
+	port: 3005,
+}
